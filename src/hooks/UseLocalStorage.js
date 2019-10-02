53c2836986a5f02a1f4efc5f4aaf5885bb4dbf96
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-02 17:32:46 +0100

    created UseLocalStorage component

---
+++ src/hooks/UseLocalStorage.js
@@ -0,0 +1,17 @@
+
+
+
+const useLocalStorage = (key, initialValue) => {
+   
+    let value = JSON.parse(localStorage.getItem(key));
+    if(!value) value = initialValue;
+    const [storedValue, setStoredValue] = useState(value);
+  
+  
+    const setValue = value => {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    };
+  
+    return [storedValue, setValue];
+  };
